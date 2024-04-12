dade4578d7ca53dad542760c4f12076efe9bea8a
GoldenHatchet15 <raphsant20@gmail.com>
2024-04-12 09:47:41 -0400

    fix pycodestyle

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,21 +4,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Return a simple string 'Hello HBNB!' when accessing the root URL."""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Return a simple string 'HBNB' when accessing the /hbnb URL."""
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def c_text(text):
-    """display custom text given"""
-    return "C {}".format(text.replace('_', ' '))
+    """Display custom text given. Replace underscores with spaces in the URL."""
+    return 'C {}'.format(text.replace('_', ' '))
+
 
 if __name__ == "__main__":
     # The application will listen on all public IPs (0.0.0.0) and port 5000
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
