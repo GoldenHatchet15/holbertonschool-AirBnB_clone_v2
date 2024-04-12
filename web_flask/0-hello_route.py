99ad53925ca99bec46fe815e49d9ec37e3a216ab
GoldenHatchet15 <raphsant20@gmail.com>
2024-04-12 03:46:27 -0400

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,4 +11,4 @@ def hello_hbnb():
 
 if __name__ == "__main__":
     # The application will listen on all public IPs (0.0.0.0) and port 5000
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
