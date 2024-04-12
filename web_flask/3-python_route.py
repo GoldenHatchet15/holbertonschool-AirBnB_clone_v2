d39048b15a8731ccdda3029b101f0ee079f3edf1
GoldenHatchet15 <raphsant20@gmail.com>
2024-04-12 10:03:17 -0400

    fix pycode

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,7 +25,7 @@ def c_is_fun(text):
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text):
-    """Display 'Python ' followed by the text or the default 'is cool', replace underscores with spaces."""
+    """Return a string 'Python is cool!'"""
     return 'Python ' + text.replace('_', ' ')
 
 
