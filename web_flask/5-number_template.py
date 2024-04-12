5abe4f43df94a491d80588c2267b30772f3e9973
GoldenHatchet15 <raphsant20@gmail.com>
2024-04-12 04:23:56 -0400

    fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -32,7 +32,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """Display an HTML page only if n is an integer."""
-    return render_template('5-number.html', number=n)
+    return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
     # The application will listen on all public IPs (0.0.0.0) and port 5000
