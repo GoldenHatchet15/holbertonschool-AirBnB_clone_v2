5f3ef539963bd80702aed588d28561a155cc661c
GoldenHatchet15 <raphsant20@gmail.com>
2024-04-12 11:46:32 -0400

    fix code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -45,8 +45,9 @@ def number_odd_or_even(n):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a HTML page with a list of all States."""
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
+    states = storage.all(State).values()  # Assuming this returns a collection of state objects.
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template("7-states_list.html", states=sorted_states)
 
 @app.teardown_appcontext
 def close_session(exception):
