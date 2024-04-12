82027779147d395c615c6af813b3391a1644441b
GoldenHatchet15 <raphsant20@gmail.com>
2024-04-12 09:33:25 -0400

    fix pycode

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Return a simple string 'Hello HBNB!' when accessing the root URL."""
     return 'Hello HBNB!'
 
+
 if __name__ == "__main__":
     # The application will listen on all public IPs (0.0.0.0) and port 5000
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
