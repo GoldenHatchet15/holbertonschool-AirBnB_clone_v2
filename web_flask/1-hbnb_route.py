8c52b3f030e786aae692f0fdf3d75a8c1f161d8c
GoldenHatchet15 <raphsant20@gmail.com>
2024-04-12 09:37:11 -0400

    fix pycode

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """A simple Flask application"""
+
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Return a simple string 'Hello HBNB!' when accessing the root URL."""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Return a simple string 'HBNB' when accessing the /hbnb URL."""
     return 'HBNB'
 
+
 if __name__ == "__main__":
     # The application will listen on all public IPs (0.0.0.0) and port 5000
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
