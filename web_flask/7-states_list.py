139e413371da63a45fb1ae1a21e5c1e5bc20225a
GoldenHatchet15 <raphsant20@gmail.com>
2024-04-12 04:59:36 -0400

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -54,5 +54,4 @@ def close_session(exception):
     storage.close()
 
 if __name__ == "__main__":
-    # The application will listen on all public IPs (0.0.0.0) and port 5000
     app.run(host='0.0.0.0', port='5000')
